763c768835c4aeb3d289e016b27534e59a37fb07
muhtasimmusfiqzarab <musfiqzarab@iut-dhaka.edu>
2019-07-13 16:08:55 +0600

    another way to configure express server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 //node path module to serve up the public directory
 const path= require('path');
 
+//get http core module
+const http= require('http'); 
+
 const express= require('express');
 
 // create express app
 const app= express();
+//we dont need to write this one as express library does this behind the scene anyways
+// we write this because we need to configure this one
+const server= http.createServer(app); // we will use server.listen is we write this line of code
 
 //port and public directory path
 const port= process.env.PORT || 3000;
@@ -14,6 +20,6 @@ const publicDirectoryPath= path.join(__dirname,'../public'); //dont have this di
 app.use(express.static(publicDirectoryPath));
 
 //start the server up
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}!`);
 })
