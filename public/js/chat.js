c454b797af42703d512faf99278863b4e02d2e3c
muhtasimmusfiqzarab <musfiqzarab@iut-dhaka.edu>
2019-07-16 13:31:45 +0600

    socket.io events

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,2 +1,15 @@
 //we can call io() function to connect
-io();
+const socket=io();
+
+//listening from server-----------recive events from server
+socket.on("countUpdated",(value)=>{
+    console.log(`The count has been updated ${value}`);
+})
+
+
+//select the button here
+document.querySelector("#increment").addEventListener('click',()=>{
+    console.log("Clicked");
+    //sending from client to server
+    socket.emit('increment');
+})
--- src/index.js
+++ src/index.js
@@ -30,8 +30,30 @@ app.use(express.static(publicDirectoryPath));
 //print message to the terminal when the client connects
 //we are listening for a given event to occur(connection)
 //connection is going to fire when socket.io server gets a new connection from client
-io.on('connection',()=>{
+
+//sending message from server to client
+let count=100;
+
+//socket is an object , contains information about the new connection
+//we use method on socket to communicate with the specific client
+io.on('connection',(socket)=>{
     console.log('new web socket connection is estublished');
+    //code to send data to the newly connection
+    //we call method on client ----sending event to client-- sending information to client
+    socket.emit('countUpdated',count);
+
+
+    //listening from client
+    socket.on("increment",()=>{
+        count++;
+        //sending the new count value
+        //socket.emit('countUpdated',count); //here we are only emiting to the specific connection , we want to emit for every connection
+
+        //io.emit emits for every single connection connected as client
+        io.emit("countUpdated",count);
+
+    })
+
 })
 //start the server up
 server.listen(port,()=>{
